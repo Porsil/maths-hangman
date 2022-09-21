a54298c842815bcfaba44c3be0340aa1e2978d38
Porsil <helenbrown13@hotmail.com>
2022-09-21 19:03:05 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import math
+import random
+
+def game_header():
+    """
+    Prints the game title and rules to the board
+    """
+    title = "\n** Maths Hangman **\n"
+    rules = "Game Rules are.....\n"
+    print(title)
+    print(rules)
+
+def game_menu():
+    """
+    Prints the game menu options to the board
+    """
+    menu = ["1. Addition"]
+    print("Game options:\n")
+    print(menu[0])
+
+
+
+
+def main():
+    """
+    Run all program functions
+    """
+    game_header()
+    game_menu()
+    choice = get_game_type()
+    game_option(choice)
+
+
+
+main()
+

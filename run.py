298ec792ce0ab19d9e9ae28dbff935c3bab7edea
Porsil <helenbrown13@hotmail.com>
2022-09-30 17:22:30 +0000

    feat: add difficulty levels to the game

--- run.py
+++ run.py
@@ -11,20 +11,20 @@ def game_header():
     print("   Select desired maths questions from the game options")
     print("   You will be given 15 questions to answer")
     print("   Each incorrect answer will add to the hangman")
-    print("   Answer all questions before the hangman is complete to win\n")
+    print("   Answer all questions before the hangman is complete to win")
 
 
 def game_menu():
     """
     Prints the game menu options to the board
     """
-    menu = ["1. Addition", "2. Subtraction", "3. Multiplication", "4. Division", "5. Quit"]
-    print("Game options:")
-    print("   " + menu[0])
-    print("   " + menu[1])
-    print("   " + menu[2])
-    print("   " + menu[3])
-    print("   " + menu[4])
+    menu_1 = ["1. Addition", "2. Subtraction", "3. Multiplication", "4. Division", "5. Quit"]
+    print("\nGame options:")
+    print("   " + menu_1[0])
+    print("   " + menu_1[1])
+    print("   " + menu_1[2])
+    print("   " + menu_1[3])
+    print("   " + menu_1[4])
 
 
 def get_game_type():
@@ -42,30 +42,89 @@ def get_game_type():
             print("\nNot a valid game option. Enter a number between 1 and 5.")
 
 
-def ask_question(index, increment, total):
+def difficulty_menu():
     """
-    Gets 2 random numbers between 1 and 25,
+    Prints the game difficulty options to the board
+    """
+    menu_2 = ["1. Easy", "2. Medium", "3. Hard", "4. Quit"]
+    print("\nDifficulty options:")
+    print("   " + menu_2[0])
+    print("   " + menu_2[1])
+    print("   " + menu_2[2])
+    print("   " + menu_2[3])
+
+
+def get_game_difficulty():
+    """
+    Asks the user for their choice of game difficulty and validates the data
+    """
+    while True:
+        try:
+            game_difficulty = int(input("\nEnter an option to play: "))
+            if game_difficulty <= 0 or game_difficulty > 4:
+                print("\nNot a valid game option. Enter a number between 1 and 4.")
+            else:
+                return game_difficulty
+        except ValueError:
+            print("\nNot a valid game option. Enter a number between 1 and 4.")
+
+
+def get_numbers_add_sub(game_difficulty):
+    """
+    Gets 2 random numbers for the addition and subtraction
+    game types, dependant on the difficulty selected
+    """
+    if game_difficulty == 1:
+        number = random.randrange(1, 26)
+        return number
+    elif game_difficulty == 2:
+        number = random.randrange(1, 101)
+        return number
+    elif game_difficulty == 3:
+        number = random.randrange(1, 501)
+        return number
+
+
+def get_numbers_mult_div(game_difficulty):
+    """
+    Gets 2 random numbers for the multiplication and division
+    game types, dependant on the difficulty selected
+    """
+    if game_difficulty == 1:
+        number = random.randrange(1, 13)
+        return number
+    elif game_difficulty == 2:
+        number = random.randrange(1, 26)
+        return number
+    elif game_difficulty == 3:
+        number = random.randrange(1, 51)
+        return number
+
+
+def ask_question(game_type, game_difficulty, increment, total):
+    """
+    Gets 2 random numbers from get_numbers functions,
     prints the appropriate maths question
     and calculates the correct answer
     """
-    if index == 1:
-        number_one = random.randrange(1, 26)
-        number_two = random.randrange(1, 26)
+    if game_type == 1:
+        number_one = get_numbers_add_sub(game_difficulty)
+        number_two = get_numbers_add_sub(game_difficulty)
         question = str(number_one) + " + " + str(number_two) + " ="
         solution = number_one + number_two
-    elif index == 2:
-        number_one = random.randrange(1, 26)
-        number_two = random.randrange(1, 26)
+    elif game_type == 2:
+        number_one = get_numbers_add_sub(game_difficulty)
+        number_two = get_numbers_add_sub(game_difficulty)
         question = str(max(number_one, number_two)) + " - " + str(min(number_one, number_two)) + " ="
         solution = max(number_one, number_two) - min(number_one, number_two)
-    elif index == 3:
-        number_one = random.randrange(1, 13)
-        number_two = random.randrange(1, 13)
+    elif game_type == 3:
+        number_one = get_numbers_mult_div(game_difficulty)
+        number_two = get_numbers_mult_div(game_difficulty)
         question = str(number_one) + " x " + str(number_two) + " ="
         solution = number_one * number_two
-    elif index == 4:
-        number_one = random.randrange(1, 13)
-        number_two = random.randrange(1, 13)
+    elif game_type == 4:
+        number_one = get_numbers_mult_div(game_difficulty)
+        number_two = get_numbers_mult_div(game_difficulty)
         question = str(number_one * number_two) + " / " + str(number_two) + " ="
         solution = (number_one * number_two) / number_two
     user_solution = get_answer(total, question)
@@ -179,10 +238,14 @@ def end_game():
     return False
 
 
-def game_play(game_type, total, correct, incorrect):
+def game_play(game_type, game_difficulty, total, correct, incorrect):
+    """
+    Loops through questions until the hangman is full
+    or 15 questions have been answered
+    """
     while incorrect < 6 and total < 15:
         total = total + 1
-        correct = ask_question(game_type, correct, total)
+        correct = ask_question(game_type, game_difficulty, correct, total)
         incorrect = total - correct
         hangman(incorrect)
         if incorrect == 6 or total == 15:
@@ -206,17 +269,13 @@ def main():
         if game_type == 5:
             play_game = end_game()
         else:
-            game_play(game_type, total, correct, incorrect)
-            #while incorrect < 6 and total < 15:
-                #total = total + 1
-                #correct = ask_question(game_type, correct, total)
-                #incorrect = total - correct
-                #hangman(incorrect)
-                #if incorrect == 6 or total == 15:
-                    #give_results(total, correct, incorrect)
-                #else:
-                    #print(f"Score: {correct}/{total}")
-            play_game = play_again()
+            difficulty_menu()
+            game_difficulty = get_game_difficulty()
+            if game_difficulty == 4:
+                play_game = end_game()
+            else:
+                game_play(game_type, game_difficulty, total, correct, incorrect)
+                play_game = play_again()
 
     
 main()
